85aed748ef2f826d5f71195664d53ee8c6b04d87
lucascarnu <lucascarnu@gmail.com>
2024-07-03 18:42:40 -0300

    Ex. 03- Criação da tela de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  formLogin!: FormGroup;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.formLogin = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      senha: new FormControl('', Validators.required),
+    });
+  }
+
+  isFieldInvalid(fieldName: string): boolean {
+    const field = this.formLogin.get(fieldName);
+    return !!field && field.invalid && (field.dirty || field.touched);
+  }
+
+  login() {
+    if (this.formLogin.valid) {
+      this.router.navigate(['/home']);
+    } else {
+      window.alert('Por favor, preencha os campos');
+    }
+  }
+
+  recuperarSenha() {
+    window.alert('Processo de recuperação de senha enviado para o e-mail cadastrado');
+  }
 }
